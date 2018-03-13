81433429c0120830f04fd399df18860c29da4bdb
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-13 22:14:45 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.highest_rating.name
+    Show.find_by(:rating => self.highest_rating).name
   end
 
   def self.lowest_rating
